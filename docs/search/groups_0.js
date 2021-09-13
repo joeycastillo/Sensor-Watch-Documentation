193baa028fa12ac36eb58875616edc03dfd9dae9
Joey Castillo <jose.castillo@gmail.com>
2021-09-13 11:50:31 -0400

    add ADC documentation

--- docs/search/groups_0.js
+++ docs/search/groups_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['analog_20input_332',['Analog Input',['../group__adc.html',1,'']]],
-  ['application_20framework_333',['Application Framework',['../group__app.html',1,'']]]
+  ['analog_20input_344',['Analog Input',['../group__adc.html',1,'']]],
+  ['application_20framework_345',['Application Framework',['../group__app.html',1,'']]]
 ];
--- docs/search/pages_1.js
+++ docs/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['todo_20list_344',['Todo List',['../todo.html',1,'']]]
+  ['todo_20list_356',['Todo List',['../todo.html',1,'']]]
 ];
