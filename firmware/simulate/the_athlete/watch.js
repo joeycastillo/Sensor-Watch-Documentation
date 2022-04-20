593e9b583dd6c9bf4c9ab89dda6f08fe3202323e
undefined <undefined>
2022-04-20 21:30:13 +0000

    Deploying to gh-pages from @ joeycastillo/Sensor-Watch-Documentation@8ce84456429958844ecaceaf7019ba8328929e31 🚀

--- firmware/simulate/the_athlete/watch.js
+++ firmware/simulate/the_athlete/watch.js
@@ -1754,21 +1754,21 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3372: function() {return Module['suspended'];},  
- 3404: function($0) {Module['suspended'] = $0;},  
- 3434: function($0) {const year = 2020 + (($0 >> 26) & 0x3f); const month = ($0 >> 22) & 0xf; const day = ($0 >> 17) & 0x1f; const hour = ($0 >> 12) & 0x1f; const minute = ($0 >> 6) & 0x3f; const second = $0 & 0x3f; const date = new Date(year, month - 1, day, hour, minute, second); return date - Date.now();},  
- 3726: function($0) {const date = new Date(Date.now() + $0); return date.getSeconds() | (date.getMinutes() << 6) | (date.getHours() << 12) | (date.getDate() << 17) | ((date.getMonth() + 1) << 22) | ((date.getFullYear() - 2020) << 26);},  
- 3944: function($0, $1, $2) {const now = Date.now(); const date = new Date(now + $0); const hour = ($1 >> 12) & 0x1f; const minute = ($1 >> 6) & 0x3f; const second = $1 & 0x3f; if ($2 == 1) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); date.setSeconds(second); } else if ($2 == 2) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); if (minute < date.getMinutes()) date.setHours(date.getHours() + 1); date.setMinutes(minute, second); } else if ($2 == 3) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); if (minute < date.getMinutes()) date.setHours(date.getHours() + 1); if (hour < date.getHours()) date.setDate(date.getDate() + 1); date.setHours(hour, minute, second); } else { throw 'Invalid alarm match mask'; } return date - now;},  
- 4728: function($0, $1) {document.querySelectorAll("[data-com='" + $0 + "'][data-seg='" + $1 + "']") .forEach((e) => e.style.opacity = 1);},  
- 4846: function($0, $1) {document.querySelectorAll("[data-com='" + $0 + "'][data-seg='" + $1 + "']") .forEach((e) => e.style.opacity = 0);},  
- 4964: function() {document.querySelectorAll("[data-com][data-seg]") .forEach((e) => e.style.opacity = 0);},  
- 5056: function($0, $1) {const classList = document.querySelector('#btn' + $0).classList; const highlight = 'highlight'; $1 ? classList.add(highlight) : classList.remove(highlight);},  
- 5217: function($0, $1) {document.getElementById('light').style.opacity = $1 / 255;},  
- 5280: function() {Module['audioContext'] = new (window.AudioContext || window.webkitAudioContext)();},  
- 5367: function() {if (Module['audioContext']) { Module['audioContext'].close(); Module['audioContext'] = null; }},  
- 5466: function($0) {const audioContext = Module['audioContext']; if (!audioContext) return; if (!(audioContext._oscillator && audioContext._gain)) { const oscillator = audioContext.createOscillator(); const gain = audioContext.createGain(); oscillator.type = 'triangle'; oscillator.connect(gain); gain.connect(audioContext.destination); oscillator.start(0); audioContext._oscillator = oscillator; audioContext._gain = gain; } audioContext._oscillator.frequency.value = 1e6/$0; audioContext._gain.gain.value = 1;},  
- 5962: function() {const audioContext = Module['audioContext']; if (audioContext && audioContext._gain) { audioContext._gain.gain.value = 0; }},  
- 6090: function() {return -new Date().getTimezoneOffset();}
+  3212: function() {return Module['suspended'];},  
+ 3244: function($0) {Module['suspended'] = $0;},  
+ 3274: function($0) {const year = 2020 + (($0 >> 26) & 0x3f); const month = ($0 >> 22) & 0xf; const day = ($0 >> 17) & 0x1f; const hour = ($0 >> 12) & 0x1f; const minute = ($0 >> 6) & 0x3f; const second = $0 & 0x3f; const date = new Date(year, month - 1, day, hour, minute, second); return date - Date.now();},  
+ 3566: function($0) {const date = new Date(Date.now() + $0); return date.getSeconds() | (date.getMinutes() << 6) | (date.getHours() << 12) | (date.getDate() << 17) | ((date.getMonth() + 1) << 22) | ((date.getFullYear() - 2020) << 26);},  
+ 3784: function($0, $1, $2) {const now = Date.now(); const date = new Date(now + $0); const hour = ($1 >> 12) & 0x1f; const minute = ($1 >> 6) & 0x3f; const second = $1 & 0x3f; if ($2 == 1) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); date.setSeconds(second); } else if ($2 == 2) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); if (minute < date.getMinutes()) date.setHours(date.getHours() + 1); date.setMinutes(minute, second); } else if ($2 == 3) { if (second < date.getSeconds()) date.setMinutes(date.getMinutes() + 1); if (minute < date.getMinutes()) date.setHours(date.getHours() + 1); if (hour < date.getHours()) date.setDate(date.getDate() + 1); date.setHours(hour, minute, second); } else { throw 'Invalid alarm match mask'; } return date - now;},  
+ 4568: function($0, $1) {document.querySelectorAll("[data-com='" + $0 + "'][data-seg='" + $1 + "']") .forEach((e) => e.style.opacity = 1);},  
+ 4686: function($0, $1) {document.querySelectorAll("[data-com='" + $0 + "'][data-seg='" + $1 + "']") .forEach((e) => e.style.opacity = 0);},  
+ 4804: function() {document.querySelectorAll("[data-com][data-seg]") .forEach((e) => e.style.opacity = 0);},  
+ 4896: function($0, $1) {const classList = document.querySelector('#btn' + $0).classList; const highlight = 'highlight'; $1 ? classList.add(highlight) : classList.remove(highlight);},  
+ 5057: function($0, $1) {document.getElementById('light').style.opacity = $1 / 255;},  
+ 5120: function() {Module['audioContext'] = new (window.AudioContext || window.webkitAudioContext)();},  
+ 5207: function() {if (Module['audioContext']) { Module['audioContext'].close(); Module['audioContext'] = null; }},  
+ 5306: function($0) {const audioContext = Module['audioContext']; if (!audioContext) return; if (!(audioContext._oscillator && audioContext._gain)) { const oscillator = audioContext.createOscillator(); const gain = audioContext.createGain(); oscillator.type = 'triangle'; oscillator.connect(gain); gain.connect(audioContext.destination); oscillator.start(0); audioContext._oscillator = oscillator; audioContext._gain = gain; } audioContext._oscillator.frequency.value = 1e6/$0; audioContext._gain.gain.value = 1;},  
+ 5802: function() {const audioContext = Module['audioContext']; if (audioContext && audioContext._gain) { audioContext._gain.gain.value = 0; }},  
+ 5930: function() {return -new Date().getTimezoneOffset();}
 };
 
 
